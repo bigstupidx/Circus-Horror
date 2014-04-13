8e1de5db9bfa3caf28361b5ac03a1dfee6e0e1e7
Juriën Meerlo <jurienmeerlo@live.nl>
2014-04-13 16:31:39 +0100

    Merged Alan's project
    Added Torch

---
+++ Circus_Horror/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	static public int health = 190;
+	static public int ammo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
