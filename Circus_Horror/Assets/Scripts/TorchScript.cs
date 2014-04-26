30b69349418354414a2de96caa2050f593afb057
owyoow <jurienmeerlo@live.nl>
2014-04-26 16:50:18 +0100

    new animations

--- Circus_Horror/Assets/Scripts/TorchScript.cs
+++ Circus_Horror/Assets/Scripts/TorchScript.cs
@@ -25,7 +25,7 @@ public class TorchScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKey(KeyCode.Q))
+		if(Input.GetKey(KeyCode.E))
 		{
 			if(torchOn && !nearLightSource && !blowOutAnim)
 			{
