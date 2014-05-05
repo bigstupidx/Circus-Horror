54643eb7f405c8427e8dab656abd9c93a00d5a3d
Juriën Meerlo <jurienmeerlo@live.nl>
2014-05-05 16:43:48 +0100

    repaired the project

--- Circus_Horror/Assets/Scripts/Follow.cs
+++ Circus_Horror/Assets/Scripts/Follow.cs
@@ -6,6 +6,10 @@ public class Follow : MonoBehaviour
 	public AudioClip[] sounds;
 	public Transform target;
 
+	public Transform firstSpawn;
+
+	public Light slenderLight;
+
 	[System.NonSerialized]
 	public bool soundIsPlaying = false;
 
@@ -16,6 +20,10 @@ public class Follow : MonoBehaviour
 	public int distanceForSoundReset = 20;
 	public int timeUntilStartChase = 30;
 
+	public Transform[] idleTargets;
+
+	ManagerScript managerScript;
+
 	bool billyHasPlayed = false;
 	float distance;
 
@@ -25,11 +33,15 @@ public class Follow : MonoBehaviour
 	float timer = 0;
 	float startTimer = 0;
 	bool chasingStarted = false;
+	bool needNewPosition = true;
 
 	// Use this for initialization
 	void Start () 
 	{
 		anim = GetComponent<Animator>();
+		anim.SetBool("ChasingPlayer", false);
+		managerScript = GameObject.Find("GameManager").GetComponent<ManagerScript>();
+		slenderLight.enabled = false;
 		anim.SetBool("ChasingPlayer", false);
 	}
 	
@@ -69,20 +81,46 @@ public class Follow : MonoBehaviour
 				timer = 0;
 				soundIsPlaying = false;
 			}
-			
+
+			if(distance < 1.5f)
+			{
+				Application.LoadLevel("MainMenu");
+			}
 			GetComponent<NavMeshAgent>().destination = target.position;
 		}
 		else
 		{
-			anim.SetBool("ChasingPlayer", false);
-			GetComponent<NavMeshAgent>().destination = transform.position;
-			if(startTimer < timeUntilStartChase)
+			if(chasingStarted)
 			{
-				startTimer += Time.deltaTime;
+				if(needNewPosition)
+				{
+					needNewPosition = false;
+					int randomNr = Random.Range (0, idleTargets.Length);
+					GetComponent<NavMeshAgent>().destination = idleTargets[randomNr].position;
+				}
+				else
+				{
+					if(GetComponent<NavMeshAgent>().remainingDistance < 5)
+					{
+						needNewPosition = true;
+					}
+				}
 			}
-			else
+
+
+			if(managerScript.slenderActive && !chasingStarted)
 			{
-				chasingStarted = true;
+				transform.position = firstSpawn.position;
+				slenderLight.enabled = true;
+				if(startTimer < timeUntilStartChase)
+				{
+					startTimer += Time.deltaTime;
+				}
+				else
+				{
+					slenderLight.enabled = false;
+					chasingStarted = true;
+				}
 			}
 		}
 	}
