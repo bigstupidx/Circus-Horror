5c4c240f7ed9a8033b81e192780a87fde151a8a7
owyoow <jurienmeerlo@live.nl>
2014-05-17 16:42:22 +0100

    added cannonball

---
+++ Circus_Horror/Assets/Scripts/CanonBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonBall : MonoBehaviour 
+{
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+	public GameObject wall;
+	public GameObject brokenDoor;
+	public Transform doorPosition;
+	bool hasTriggered = false;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.U) && !hasTriggered)
+		{
+			Destroy(leftDoor);
+			Destroy(rightDoor);
+			Instantiate(brokenDoor, doorPosition.position, doorPosition.rotation);
+
+			wall.collider.enabled = false;
+			hasTriggered = true;
+			rigidbody.isKinematic = false;
+			rigidbody.AddForce(new Vector3(-8000, 0, 0));
+		}
+	}
+}
